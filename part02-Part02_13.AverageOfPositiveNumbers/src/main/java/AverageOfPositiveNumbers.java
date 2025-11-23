68d601300098ea579fccdd0edd41dfcfea8eba60
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-11-23 12:13:07 +0100

    added part02-13

---
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int counter = 0;
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            } else if (input > 0) {
+                sum += input;
+                counter++;
+            } else {
+                continue;
+            }
+        }
+        double average = (double) sum / counter;
+
+        if (average > 0) {
+            System.out.println(average);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+    }
+}
