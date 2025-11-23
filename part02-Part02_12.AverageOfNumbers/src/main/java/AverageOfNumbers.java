df48f68b9e7ae85cd226022e5f2b045f8fc7e6cb
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-11-23 12:07:21 +0100

    added part02-12

---
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int counter = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            sum += input;
+            counter++;
+        }
+        
+        double average = (double) sum / counter;
+
+        System.out.println("Average of the numbers: " + average);
+    }
+}
