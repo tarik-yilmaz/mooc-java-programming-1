1d6310bcd5e3dbfe4028d8b16c61f41338ece6c2
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-11-24 22:24:38 +0100

    added part02-16

---
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class FromWhereToWhere {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Part 1 & Part 2
+        System.out.print("Where to? ");
+        int end = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Where from? ");
+        int start = Integer.valueOf(scanner.nextLine());
+
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
+}
