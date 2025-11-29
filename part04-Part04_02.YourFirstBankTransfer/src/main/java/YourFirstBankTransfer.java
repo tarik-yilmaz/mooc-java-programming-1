d9e0ab18e1298c88ec7e1885837e00d55a7a4ce1
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-11-29 10:09:05 +0100

    added part04-02

---
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,18 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        Account matthewAccount = new Account("Matthews account", 1000);
+
+        Account myAccount = new Account("My account", 0);
+
+        matthewAccount.withdrawal(100.0);
+
+        myAccount.deposit(100.0);
+
+        System.out.println(matthewAccount.toString());
+        System.out.println(myAccount.toString());
+    }
+}
