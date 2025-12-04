0e4dcae1c458d7125da951707bfbca4f47106381
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-04 15:41:19 +0100

    added part04-28

---
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -0,0 +1,34 @@
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+
+        int counter = 0;
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+
+            while (fileScanner.hasNextLine()) {
+                int numberAtRow = Integer.valueOf(fileScanner.nextLine());
+
+                if (numberAtRow >= lowerBound && numberAtRow <= upperBound) {
+                    counter++;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Numbers: " + counter);
+    }
+}
