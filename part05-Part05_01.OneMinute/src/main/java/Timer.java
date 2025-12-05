111ee80d5f2fa94cf215a56d826f61869131cf59
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-05 12:53:08 +0100

    added part05-01

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,22 @@
+public class Timer {
+    private ClockHand seconds;
+    private ClockHand hundredsOfSeconds;
+
+    public Timer() {
+        this.seconds = new ClockHand(60);
+        this.hundredsOfSeconds = new ClockHand(100);
+    }
+
+    public void advance() {
+        this.hundredsOfSeconds.advance();
+
+        if (this.hundredsOfSeconds.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+
+    public String toString() {
+       return seconds + ":" + hundredsOfSeconds;
+    }
+
+}
