332127f1bdbbf55caa238d5ba0cbfce9a62bb85e
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-12-06 09:22:18 +0100

    added part05-02

---
+++ part05-Part05_02.Book/src/main/java/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String author;
+    private String title;
+    private int pageCount;
+
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.title = name;
+        this.pageCount = pages;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getName() {
+        return this.title;
+    }
+
+    public int  getPages() {
+        return this.pageCount;
+    }
+
+    @Override
+    public String toString() {
+        return this.author + ", " + this.title + ", " + this.pageCount + " pages";
+    }
+
+}
